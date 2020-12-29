625138a1b2d627e7fb8c6c7a791990c7ee7b8813
toannd <sitdownrightnow.dev@gmail.com>
2020-12-29 10:55:44 +0700

    refactor: fix naming convention of *Action class

--- domain/src/main/java/library/book/core/port/incoming/QueryBookUseCase.java
+++ domain/src/main/java/library/book/core/port/incoming/QueryBookUseCase.java
@@ -1,11 +1,11 @@
 package library.book.core.port.incoming;
 
 import library.book.core.model.Book;
-import library.book.core.model.action.BookQueryAction;
+import library.book.core.model.action.QueryBookAction;
 
 import java.util.List;
 
 public interface QueryBookUseCase {
-    List<Book> getAllBooks(BookQueryAction bookQueryAction);
+    List<Book> getAllBooks(QueryBookAction queryBookAction);
 
 }
