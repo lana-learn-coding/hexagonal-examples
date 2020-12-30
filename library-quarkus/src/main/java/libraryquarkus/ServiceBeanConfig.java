d6c5efad579e9b57e3859aeb6800cd45e70c5679
toannd <sitdownrightnow.dev@gmail.com>
2020-12-30 09:38:31 +0700

    dev: declare service bean in config

--- library-quarkus/src/main/java/libraryquarkus/ServiceBeanConfig.java
+++ library-quarkus/src/main/java/libraryquarkus/ServiceBeanConfig.java
@@ -1,24 +1,19 @@
-package libraryspring;
+package libraryquarkus;
 
 import library.book.core.BookService;
 import library.reader.core.ReaderService;
 import library.reader.core.port.outgoing.BookRepo;
 import library.reader.core.port.outgoing.ReaderRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import javax.enterprise.inject.Default;
+
 public class ServiceBeanConfig {
-
-    @Bean
-    @Autowired
+    @Default
     public BookService bookService(library.book.core.port.outgoing.BookRepo bookRepo) {
         return new BookService(bookRepo);
     }
 
-    @Bean
-    @Autowired
+    @Default
     public ReaderService readerService(BookRepo bookRepo, ReaderRepo readerRepo) {
         return new ReaderService(bookRepo, readerRepo);
     }
